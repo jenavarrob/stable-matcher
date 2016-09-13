f04beb95b078617db829c9abb683a05d3c6f8fb6
Jesus <jenavarrob@gmail.com>
2016-09-13 19:41:25 +0200

    some refactoring and organization.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # stable-matcher
-Project to work with the Stable Matching algorithm.
+Finds stable matching for two parties with a set of preferences. This code is for illustrative and learning purposes.
 
 The Stable Matching algorithm used in this project is the Gale-Shapley algorithm which is very simple but very powerful algorithm to find stable matches for two parties each with a set of preferences. It was first introduced by D. Gale and L.S. Shapley in the article "College Admissions and the Stability of Marriage" published in 1962 in "The American Mathematical Monthly" (see http://www.jstor.org/stable/2312726). This algorithm and the theory behind it are so helpful and interesting that led Lloyd S. Shapley together with Alvin E. Roth to win the Nobel Prize in Economics in 2012 "for the theory of stable allocations and the practice of market design" (see http://www.nobelprize.org/nobel_prizes/economics/laureates/2012/).
+
+## Setup mono
+Follow the instructions here to install mono:
+```
+http://www.mono-project.com/docs/getting-started/install/linux/
+```
+
+Do not forget to install also these other packages:
+```
+sudo apt install mono-mcs
+sudo apt install mono-devel
+```
+
+## How to compile and run
+Compile:
+```
+mcs stableMatcher.cs utils.cs
+```
+Run:
+```
+mono stableMatcher.exe
+```
 
--- stableMatcher.cs
+++ stableMatcher.cs
@@ -1,119 +1,38 @@
 using System;
- 
-public class StableMatcher
-{
-    static public void Main ()
-    {
-        Console.WriteLine ("Gale-Shapley Stable Matching algorithm");
-	int[,] menPrefs = loadDemoPrefs(1);
-	int[,] womenPrefs = loadDemoPrefs(2);
-	
-	Console.WriteLine ("Show preferences for men");
-	showPrefs(menPrefs);
-	Console.WriteLine ("Show preferences for women");
-	showPrefs(womenPrefs);
 
-	int[] fiancee = runStableMatchingAlgorithm(menPrefs, womenPrefs);
-	//show results            
-        Console.WriteLine("Solution to stable matiching problem:");
-        for (int i = 0; i < fiancee.Length; i++){
-            Console.Write("W{0}-M{1}, ", i, fiancee[i]);
-        }
-    }
+namespace StableMatchingAlgorithm {
+  public class StableMatcher
+  {
+      static public void Main ()
+      {
+	  Console.WriteLine ("Gale-Shapley Stable Matching algorithm");
+	  int[,] menPrefs = Examples.loadPrefsSize5(1);
+	  int[,] womenPrefs = Examples.loadPrefsSize5(2);
 
-    //Stable Matching Algorithm
-    static public int[] runStableMatchingAlgorithm(int[,] menPrefs, int[,] womenPrefs)
-    {
-	int size = menPrefs.GetLength(1);
-	int[,] rank = getRankedMatrixWithDummy(womenPrefs);
+	  Console.WriteLine ("Show preferences for men");
+	  showPrefs(menPrefs);
+	  Console.WriteLine ("Show preferences for women");
+	  showPrefs(womenPrefs);
 
-	//start stable marriage algorithm
-	int[] fiancee = new int[size];
-	int[] next = new int[size];  //to track the next in the list of women candidates
+	  int[] fiancee = Utils.findStableMatchesUsingGaleShapleyAlgorithm(menPrefs, womenPrefs);
+	  //show results            
+	  Console.WriteLine("Solution to stable matiching problem:");
+	  for (int i = 0; i < fiancee.Length; i++){
+	      Console.Write("W{0}-M{1}, ", i, fiancee[i]);
+	  }
+	  Console.WriteLine();
+      }
 
-	for (int i = 0; i < size; i++){  //start counters to zero index
-	    fiancee[i] = size;
-	    next[i] = -1;
-	}
-
-	for (int m = 0; m < size; m++){
-	    int s = m;
-
-	    while (s != size){
-		next[s] = next[s] + 1;
-		int w = menPrefs[s, next[s]];
-		if (rank[w, s] < rank[w, fiancee[w]]){
-		    int t = fiancee[w];
-		    fiancee[w] = s;
-		    s = t;
-		}
-	    }
-	}
-
-	return fiancee;
-    }
-
-    /** Converts from a preference matrix to a ranked-preference matrix, i.e. 
-     * converts from vector elements showing preferences sorted by column
-     * to vector elements were each element shows their particular order in the array 
-     * Example: Prefs = [4,0,3,1,2] -> RankedPrefs = [1,3,4,2,0]
-     */
-    static public int[,] getRankedMatrixWithDummy(int[,] preferenceMatrix)
-    {
-	int size = preferenceMatrix.GetLength(1);
-
-	//add extra column to rank for initialization with a dummy
-	int[,] rank = new int[size, size + 1];
-
-	//convert from a preference matrix to a ranked-preference matrix
-	for (int i = 0; i < size; i++){
-	    for (int j = 0; j < size; j++){
-		rank[i, preferenceMatrix[i, j]] = j;
-	    }
-	}
-
-	//initialize dummy column with largest possible value
-	for (int i = 0; i < size; i++){
-	    rank[i, size] = size;
-	}
-
-	return rank;
-    }
-
-
-    static public int[,] loadDemoPrefs(int option)
-    {
-	const int size = 5;
-        int[,] prefs = null;
-	
-	if (option == 1){
-	   prefs = new int[size, size]{
-	   {1, 4, 0, 2, 3},
-	   {0, 1, 2, 3, 4},
-	   {1, 2, 4, 3, 0},
-	   {0, 2, 1, 3, 4},
-	   {4, 2, 1, 0, 3}};
-	}
-	else if(option == 2){
-	   prefs = new int[size,size]{
-	   {4, 0, 3, 1, 2},
-  	   {3, 4, 1, 0, 2},
-	   {0, 3, 1, 2, 4},
-  	   {2, 1, 3, 0, 4},
-	   {3, 1, 2, 4, 0}};
-	}
-        return prefs;
-    }
-    
-    static public void showPrefs(int[,] prefs)
-    {
-        int size = prefs.GetLength(0);
-	for(int row = 0; row < size; row++){
-	    Console.Write("{0}: ", (row).ToString());
-	    for(int col = 0; col < size; col++){
-		Console.Write("{0} ", prefs[row,col].ToString());
-	    }
-	    Console.WriteLine("");
-	}
-    }
+      static public void showPrefs(int[,] prefs)
+      {
+	  int size = prefs.GetLength(0);
+	  for(int row = 0; row < size; row++){
+	      Console.Write("{0}: ", (row).ToString());
+	      for(int col = 0; col < size; col++){
+		  Console.Write("{0} ", prefs[row,col].ToString());
+	      }
+	      Console.WriteLine("");
+	  }
+      }
+  }
 }
