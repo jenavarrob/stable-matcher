37f661296c61b963cdf6359c33f63da1bad91d09
Niklas <garnix.gut@web.de>
2019-10-10 10:32:55 +0200

    fixed indentation - fixed general format to be readable in vs code

--- stableMatcher.cs
+++ stableMatcher.cs
@@ -1,38 +1,35 @@
 using System;
 
 namespace StableMatchingAlgorithm {
-  public class StableMatcher
-  {
-      static public void Main ()
-      {
-	  Console.WriteLine ("Gale-Shapley Stable Matching algorithm");
-	  int[,] menPrefs = Examples.loadPrefsSize5(1);
-	  int[,] womenPrefs = Examples.loadPrefsSize5(2);
+	public class StableMatcher {
+		static public void Main () {
+			Console.WriteLine ("Gale-Shapley Stable Matching algorithm");
+			int[,] menPrefs = Examples.loadPrefsSize5 (1);
+			int[,] womenPrefs = Examples.loadPrefsSize5 (2);
 
-	  Console.WriteLine ("Show preferences for men");
-	  showPrefs(menPrefs);
-	  Console.WriteLine ("Show preferences for women");
-	  showPrefs(womenPrefs);
+			Console.WriteLine ("Show preferences for men");
+			showPrefs (menPrefs);
+			Console.WriteLine ("Show preferences for women");
+			showPrefs (womenPrefs);
 
-	  int[] fiancee = Utils.findStableMatchesUsingGaleShapleyAlgorithm(menPrefs, womenPrefs);
-	  //show results            
-	  Console.WriteLine("Solution to stable matiching problem:");
-	  for (int i = 0; i < fiancee.Length; i++){
-	      Console.Write("W{0}-M{1}, ", i, fiancee[i]);
-	  }
-	  Console.WriteLine();
-      }
+			int[] fiancee = Utils.findStableMatchesUsingGaleShapleyAlgorithm (menPrefs, womenPrefs);
+			//show results            
+			Console.WriteLine ("Solution to stable matiching problem:");
+			for (int i = 0; i < fiancee.Length; i++) {
+				Console.Write ("W{0}-M{1}, ", i, fiancee[i]);
+			}
+			Console.WriteLine ();
+		}
 
-      static public void showPrefs(int[,] prefs)
-      {
-	  int size = prefs.GetLength(0);
-	  for(int row = 0; row < size; row++){
-	      Console.Write("{0}: ", (row).ToString());
-	      for(int col = 0; col < size; col++){
-		  Console.Write("{0} ", prefs[row,col].ToString());
-	      }
-	      Console.WriteLine("");
-	  }
-      }
-  }
+		static public void showPrefs (int[,] prefs) {
+			int size = prefs.GetLength (0);
+			for (int row = 0; row < size; row++) {
+				Console.Write ("{0}: ", (row).ToString ());
+				for (int col = 0; col < size; col++) {
+					Console.Write ("{0} ", prefs[row, col].ToString ());
+				}
+				Console.WriteLine ("");
+			}
+		}
+	}
 }
