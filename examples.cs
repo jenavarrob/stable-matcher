a3e41f394265862c1bc417712de3d5c80e1f51f0
Niklas <garnix.gut@web.de>
2019-10-10 10:37:13 +0200

    testing better indentation in github

--- examples.cs
+++ examples.cs
@@ -1,27 +1,27 @@
 namespace StableMatchingAlgorithm {
-	static public class Examples {
-		static public int[,] loadPrefsSize5 (int option) {
-			const int size = 5;
-			int[,] prefs = null;
+   static public class Examples {
+      static public int[,] loadPrefsSize5 (int option) {
+         const int size = 5;
+         int[,] prefs = null;
 
-			if (option == 1) {
-				prefs = new int[size, size] { 
-					{ 1, 4, 0, 2, 3 }, 
-					{ 0, 1, 2, 3, 4 }, 
-					{ 1, 2, 4, 3, 0 }, 
-					{ 0, 2, 1, 3, 4 }, 
-					{ 4, 2, 1, 0, 3 }
-				};
-			} else if (option == 2) {
-				prefs = new int[size, size] { 
-					{ 4, 0, 3, 1, 2 }, 
-					{ 3, 4, 1, 0, 2 }, 
-					{ 0, 3, 1, 2, 4 }, 
-					{ 2, 1, 3, 0, 4 }, 
-					{ 3, 1, 2, 4, 0 }
-				};
-			}
-			return prefs;
-		}
-	}
+         if (option == 1) {
+            prefs = new int[size, size] { 
+               { 1, 4, 0, 2, 3 }, 
+               { 0, 1, 2, 3, 4 }, 
+               { 1, 2, 4, 3, 0 }, 
+               { 0, 2, 1, 3, 4 }, 
+               { 4, 2, 1, 0, 3 }
+            };
+         } else if (option == 2) {
+            prefs = new int[size, size] { 
+               { 4, 0, 3, 1, 2 }, 
+               { 3, 4, 1, 0, 2 }, 
+               { 0, 3, 1, 2, 4 }, 
+               { 2, 1, 3, 0, 4 }, 
+               { 3, 1, 2, 4, 0 }
+            };
+         }
+         return prefs;
+      }
+   }
 }
